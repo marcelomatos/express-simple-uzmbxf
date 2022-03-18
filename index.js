9bcaf1e51ca0dcd364e5ee47096ddc1ca33b3a6f
marcelomatos <mmatos.mmds@gmail.com>
2022-03-18 01:35:58 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3010;
+const path = require('path');
+
+app.use(express.static('static'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve('pages/index.html'));
+});
+app.get('/appdb', (req, res) => {
+  res.sendFile(path.resolve('pages/index2.html'));
+});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
